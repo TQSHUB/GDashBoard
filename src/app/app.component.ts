c1555899a0ba826f4dc3902c1f3f145dd4d167a9
techqsoft <vaidyakashyap84@gmail.com>
2017-05-27 13:17:25 +0530

    Add some keywords

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'This is an Angular!';
+  title = 'This  fff is an Angular!';
 }
